0e7259e7538d3b85f9306a8253baa0d995897ecc
rpnayak80 <rpnayak80@gmail.com>
2019-08-29 12:54:20 -0400

    update

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -8,7 +8,7 @@ const LaunchRequestHandler = {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
     },
     handle(handlerInput) {
-        const speakOutput = 'Welcome, you can say ITSS or USDC. Which would you like to try?';
+        const speakOutput = 'Welcome, you can go to 3rd floor to meet teams. Which would you like to try?';
         return handlerInput.responseBuilder
             .speak(speakOutput)
             .reprompt(speakOutput)
